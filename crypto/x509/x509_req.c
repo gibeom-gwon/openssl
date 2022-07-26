61e2e5a9e8432a7e2b1f32c8729c552ce3efefda
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-07-26 21:45:42 +0900

    Flag modified when X509_req_info_st attribute updated
    We need to reencode X509_req_info_st if attribute updated.

--- crypto/x509/x509_req.c
+++ crypto/x509/x509_req.c
@@ -214,11 +214,13 @@ X509_ATTRIBUTE *X509_REQ_get_attr(const X509_REQ *req, int loc)
 
 X509_ATTRIBUTE *X509_REQ_delete_attr(X509_REQ *req, int loc)
 {
+    req->req_info.enc.modified = 1;
     return X509at_delete_attr(req->req_info.attributes, loc);
 }
 
 int X509_REQ_add1_attr(X509_REQ *req, X509_ATTRIBUTE *attr)
 {
+    req->req_info.enc.modified = 1;
     if (X509at_add1_attr(&req->req_info.attributes, attr))
         return 1;
     return 0;
@@ -228,6 +230,7 @@ int X509_REQ_add1_attr_by_OBJ(X509_REQ *req,
                               const ASN1_OBJECT *obj, int type,
                               const unsigned char *bytes, int len)
 {
+    req->req_info.enc.modified = 1;
     if (X509at_add1_attr_by_OBJ(&req->req_info.attributes, obj,
                                 type, bytes, len))
         return 1;
@@ -238,6 +241,7 @@ int X509_REQ_add1_attr_by_NID(X509_REQ *req,
                               int nid, int type,
                               const unsigned char *bytes, int len)
 {
+    req->req_info.enc.modified = 1;
     if (X509at_add1_attr_by_NID(&req->req_info.attributes, nid,
                                 type, bytes, len))
         return 1;
@@ -248,6 +252,7 @@ int X509_REQ_add1_attr_by_txt(X509_REQ *req,
                               const char *attrname, int type,
                               const unsigned char *bytes, int len)
 {
+    req->req_info.enc.modified = 1;
     if (X509at_add1_attr_by_txt(&req->req_info.attributes, attrname,
                                 type, bytes, len))
         return 1;
